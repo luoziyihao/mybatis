936c8a1e8775be092bb7aae8a1321da1e950b713
luoziyihao <wangyiraoxiang@163.com>
2016-04-26 21:04:18 -0400

    my mybatis code create 0.1

---
+++ src/main/java/me/mybatis/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package me.mybatis.dao;
+
+import me.mybatis.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
